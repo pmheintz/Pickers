c741c52ebfbb24fdff57e83e155206ce2cae5f87
Paul Heintz <pheintz@Pauls-MacBook-Pro.local>
2017-04-09 04:02:42 -0500

    Finished app... swift sucks ;)

--- Pickers/DependentComponentPickerViewController.swift
+++ Pickers/DependentComponentPickerViewController.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 
-class DependentComponentPickerViewController: UIViewController {
+class DependentComponentPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var dependentPicker: UIPickerView!
+    private let stateComponent = 0
+    private let zipComponent = 1
+    private var stateZips:[String : [String]]!
+    private var states:[String]!
+    private var zips:[String]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let bundle = Bundle.main
+        let plistURL = bundle.url(forResource: "statedictionary", withExtension: "plist")
+        stateZips = NSDictionary.init(contentsOf: (plistURL)!) as! [String : [String]]
+        let allStates = stateZips.keys
+        states = allStates.sorted()
+        let selectedState = states[0]
+        zips = stateZips[selectedState]
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,4 +45,61 @@ class DependentComponentPickerViewController: UIViewController {
     }
     */
 
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let stateRow = dependentPicker.selectedRow(inComponent: stateComponent)
+        let zipRow = dependentPicker.selectedRow(inComponent: zipComponent)
+        let state = states[stateRow]
+        let zip = zips[zipRow]
+        
+        let title = "You selected zip code \(zip)"
+        let message = "\(zip) is in \(state)"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: -
+    // MARK: Picker Data Source Methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == stateComponent {
+            return states.count
+        } else {
+            return zips.count
+        }
+    }
+    
+    // MARK: -
+    //MARK: Picker Delegate Methods
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        let pickerWidth = pickerView.bounds.size.width
+        if component == zipComponent {
+            return pickerWidth/3
+        } else {
+            return 2 * pickerWidth/3
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == stateComponent {
+            return states[row]
+        } else {
+            return zips[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == stateComponent {
+            let selectedState = states[row]
+            zips = stateZips[selectedState]
+            dependentPicker.reloadComponent(zipComponent)
+            dependentPicker.selectRow(0, inComponent: zipComponent, animated: true)
+        }
+    }
 }
--- Pickers/DoubleComponentPickerViewController.swift
+++ Pickers/DoubleComponentPickerViewController.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
-class DoubleComponentPickerViewController: UIViewController {
+class DoubleComponentPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var doublePicker: UIPickerView!
+    private let fillingComponent = 0
+    private let breadComponent = 1
+    private let fillingTypes = ["Peanut Butter", "Tuna Salad", "Chicken Salad", "Roast Beef", "Vegemite"]
+    private let breadTypes = ["White", "Whole Wheet", "Rye", "Sourdough", "Seven Grain"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,5 +36,42 @@ class DoubleComponentPickerViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let fillingRow = doublePicker.selectedRow(inComponent: fillingComponent)
+        let breadRow = doublePicker.selectedRow(inComponent: breadComponent)
+        let filling = fillingTypes[fillingRow]
+        let bread = breadTypes[breadRow]
+        let message = "Your \(filling) on \(bread) bread will be right up."
+        
+        let alert = UIAlertController(title: "Thank you for your order", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Great", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: -
+    // MARK: Picker Data Source Methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == breadComponent {
+            return breadTypes.count
+        } else {
+            return fillingTypes.count
+        }
+    }
+    
+    // MARK: Picker Delegate Methods
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == breadComponent {
+            return breadTypes[row]
+        } else {
+            return fillingTypes[row]
+        }
+    }
 
 }
