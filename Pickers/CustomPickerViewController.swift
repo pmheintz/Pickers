83d756af7253f3d9c7e92b12b415c2a6e8ee2cfe
Paul Heintz <pheintz@Pauls-MacBook-Pro.local>
2017-04-09 04:03:35 -0500

    Missed a class...

--- Pickers/CustomPickerViewController.swift
+++ Pickers/CustomPickerViewController.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
+import AudioToolbox
 
-class CustomPickerViewController: UIViewController {
+class CustomPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var winLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    private var images:[UIImage]!
+    private var winSoundID: SystemSoundID = 0
+    private var crunchSoundID: SystemSoundID = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        images = [UIImage(named: "seven")!, UIImage(named: "bar")!, UIImage(named: "crown")!, UIImage(named: "cherry")!, UIImage(named: "lemon")!, UIImage(named: "apple")!]
+        winLabel.text = " "
+        arc4random_stir()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,5 +41,81 @@ class CustomPickerViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func spin(_ sender: UIButton) {
+        var win = false
+        var numInRow = -1
+        var lastVal = -1
+        
+        for i in 0..<5 {
+            let newValue = Int(arc4random_uniform(UInt32(images.count)))
+            if newValue == lastVal {
+                numInRow += 1
+            } else {
+                numInRow = 1
+            }
+            lastVal = newValue
+            
+            picker.selectRow(newValue, inComponent: i, animated: true)
+            picker.reloadComponent(i)
+            if numInRow >= 3 {
+                win = true
+            }
+        }
+        
+        if crunchSoundID == 0 {
+            let soundUrl = Bundle.main.url(forResource: "crunch", withExtension: "wav")! as CFURL
+            AudioServicesCreateSystemSoundID(soundUrl, &crunchSoundID)
+        }
+        AudioServicesPlaySystemSound(crunchSoundID)
+        
+        if win {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.playWinSound()
+            }
+        } else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.showButton()
+            }
+        }
+        button.isHidden = true
+        winLabel.text = " "//win ? "WINNER!  " : " "
+    }
+    
+    func showButton() {
+        button.isHidden = false
+    }
+    
+    func playWinSound() {
+        if winSoundID == 0 {
+            let soundURL = Bundle.main.url(forResource: "win", withExtension: "wav")! as CFURL
+            AudioServicesCreateSystemSoundID(soundURL, &winSoundID)
+        }
+        AudioServicesPlaySystemSound(winSoundID)
+        winLabel.text = "Winner! "
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            self.showButton()
+        }
+    }
+    
+    // MARK: -
+    // MARK: Picker Data Source Methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 5
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return images.count
+    }
+    
+    // MARK: -
+    // MARK: Picker Delegate Methods
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let image = images[row]
+        let imageView = UIImageView(image: image)
+        return imageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 64
+    }
 }
